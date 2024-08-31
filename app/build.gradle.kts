2d818a7a6713300b4965f98d68f442d915a012a1
Rahmat Trinanda <rahmat3nanda@gmail.com>
2024-09-01 00:00:06 +0700

    add gson

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
     implementation(libs.rcache.android)
     implementation(libs.androidx.security.crypto)
 
+    implementation("com.google.code.gson:gson:2.10.1")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
