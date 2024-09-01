1f8bc4e7180a39ed54570c245d76cd26c08ef6d4
Rahmat Trinanda <rahmat3nanda@gmail.com>
2024-09-02 00:18:21 +0700

    Create KeyContract

---
+++ app/src/main/java/id/nesd/rcache/demo/KeyContract.kt
@@ -0,0 +1,23 @@
+package id.nesd.rcache.demo
+
+import id.nesd.rcache.demo.models.KeyItem
+
+interface KeyContract {
+    interface View {
+        fun dataChanged(items: List<KeyItem>)
+    }
+
+    interface Model<T> {
+        val items: MutableList<T>
+
+        fun loadItems()
+        fun add(item: T)
+        fun delete(at: Int)
+    }
+
+    interface Presenter {
+        fun loadItems()
+        fun add(item: String)
+        fun delete(item: KeyItem)
+    }
+}
