effbae07449af77b0f59ea1e2841d022431c2311
Rahmat Trinanda <rahmat3nanda@gmail.com>
2024-09-01 17:51:46 +0700

    Create KeyActivity

---
+++ app/src/main/java/id/nesd/rcache/demo/KeyActivity.kt
@@ -0,0 +1,23 @@
+package id.nesd.rcache.demo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import id.nesd.rcache.demo.ui.theme.RCacheDemoAndroidTheme
+
+class KeyActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            RCacheDemoAndroidTheme {
+                Scaffold { innerPadding ->
+                    Text("tes")
+                }
+            }
+        }
+    }
+}
