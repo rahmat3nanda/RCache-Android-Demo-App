d4b0659227bafd47c00227ef98d91f7a21be9a83
Rahmat Trinanda <rahmat3nanda@gmail.com>
2024-09-02 00:18:43 +0700

    Create Key Model & Presenter

---
+++ app/src/main/java/id/nesd/rcache/demo/models/KeyModel.kt
@@ -0,0 +1,33 @@
+package id.nesd.rcache.demo.models
+
+import id.nesd.rcache.RCache
+import id.nesd.rcache.demo.AppRCacheKey
+import id.nesd.rcache.demo.KeyContract
+import id.nesd.rcache.demo.utils.LogActionType
+import id.nesd.rcache.demo.utils.LogManager
+
+data class KeyItem(val name: String)
+
+class KeyModel : KeyContract.Model<KeyItem> {
+    override val items: MutableList<KeyItem> = mutableListOf()
+
+    override fun loadItems() {
+        val keys: List<String>? = RCache.common.readArray(AppRCacheKey.savedKeys) as? List<String>?
+        if (keys != null) {
+            items.clear()
+            items.addAll(keys.map { KeyItem(name = it) })
+        }
+    }
+
+    override fun add(item: KeyItem) {
+        items.add(item)
+        RCache.common.save(array = items.map { it.name }, key = AppRCacheKey.savedKeys)
+        LogManager.instance.add(action = LogActionType.ADD_KEY, value = item.name)
+    }
+
+    override fun delete(at: Int) {
+        LogManager.instance.add(action = LogActionType.REMOVE_KEY, value = items[at].name)
+        items.removeAt(at)
+        RCache.common.save(array = items.map { it.name }, key = AppRCacheKey.savedKeys)
+    }
+}
---
+++ app/src/main/java/id/nesd/rcache/demo/presenters/KeyPresenter.kt
@@ -0,0 +1,24 @@
+package id.nesd.rcache.demo.presenters
+
+import id.nesd.rcache.demo.KeyContract
+import id.nesd.rcache.demo.models.KeyItem
+
+class KeyPresenter(
+    private var view: KeyContract.View,
+    private val model: KeyContract.Model<KeyItem>,
+) : KeyContract.Presenter {
+    override fun loadItems() {
+        model.loadItems()
+        view.dataChanged(model.items)
+    }
+
+    override fun add(item: String) {
+        model.add(KeyItem(name = item))
+        view.dataChanged(model.items)
+    }
+
+    override fun delete(item: KeyItem) {
+        model.delete(model.items.indexOf(item))
+        view.dataChanged(model.items)
+    }
+}
