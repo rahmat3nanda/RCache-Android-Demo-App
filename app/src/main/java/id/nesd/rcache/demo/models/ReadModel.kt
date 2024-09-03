afb74266776308a37a1d68a356f6cbc831c29960
Rahmat Trinanda <rahmat3nanda@gmail.com>
2024-09-03 23:38:29 +0700

    implement add to log

--- app/src/main/java/id/nesd/rcache/demo/models/ReadModel.kt
+++ app/src/main/java/id/nesd/rcache/demo/models/ReadModel.kt
@@ -1,6 +1,8 @@
 package id.nesd.rcache.demo.models
 
 import id.nesd.rcache.demo.contracts.ReadContract
+import id.nesd.rcache.demo.utils.LogActionType
+import id.nesd.rcache.demo.utils.LogManager
 
 class ReadModel : ReadContract.Model {
     override fun read(
@@ -12,35 +14,59 @@ class ReadModel : ReadContract.Model {
         when (dataType) {
             DataType.BYTE_ARRAY -> {
                 val value = storageType.rCache().readByteArray(key = key.rCacheKey())
-                completion(String(bytes = value ?: "null".toByteArray()))
+                val string = String(bytes = value ?: "null".toByteArray())
+                completion(string)
+                addToLog(dataType = dataType, key = key, storageType = storageType, value = string)
             }
 
             DataType.STRING -> {
                 val value = storageType.rCache().readString(key = key.rCacheKey())
-                completion(value ?: "null")
+                val string = value ?: "null"
+                completion(string)
+                addToLog(dataType = dataType, key = key, storageType = storageType, value = string)
             }
 
             DataType.BOOL -> {
                 val value = storageType.rCache().readBool(key = key.rCacheKey())
-                completion(value?.toString() ?: "null")
+                val string = value?.toString() ?: "null"
+                completion(string)
+                addToLog(dataType = dataType, key = key, storageType = storageType, value = string)
             }
 
             DataType.INTEGER -> {
                 val value = storageType.rCache().readInteger(key = key.rCacheKey())
-                completion(value?.toString() ?: "null")
+                val string = value?.toString() ?: "null"
+                completion(string)
+                addToLog(dataType = dataType, key = key, storageType = storageType, value = string)
             }
 
-            DataType.ARRAY, DataType.MAP -> TODO()
+            DataType.ARRAY, DataType.MAP -> completion("Not Implemented")
 
             DataType.DOUBLE -> {
                 val value = storageType.rCache().readDouble(key = key.rCacheKey())
-                completion(value?.toString() ?: "null")
+                val string = value?.toString() ?: "null"
+                completion(string)
+                addToLog(dataType = dataType, key = key, storageType = storageType, value = string)
             }
 
             DataType.FLOAT -> {
                 val value = storageType.rCache().readFloat(key = key.rCacheKey())
-                completion(value?.toString() ?: "null")
+                val string = value?.toString() ?: "null"
+                completion(string)
+                addToLog(dataType = dataType, key = key, storageType = storageType, value = string)
             }
         }
+    }
+
+    private fun addToLog(
+        dataType: DataType,
+        key: KeyItem,
+        storageType: StorageType,
+        value: String
+    ) {
+        LogManager.instance.add(
+            action = LogActionType.READ,
+            value = "\n-Data Type: ${dataType.stringValue}\n-Key: ${key.name}\n-Storage Type: ${storageType.stringValue}\n-Value: $value"
+        )
     }
 }
