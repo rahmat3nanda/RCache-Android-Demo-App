99bc5c770aee97ea888d8e0b5a6b47bd27a1718e
Rahmat Trinanda <rahmat3nanda@gmail.com>
2024-09-04 00:11:08 +0700

    Create Clear Model

--- app/src/main/java/id/nesd/rcache/demo/models/ClearModel.kt
+++ app/src/main/java/id/nesd/rcache/demo/models/ClearModel.kt
@@ -1,5 +1,10 @@
 package id.nesd.rcache.demo.models
 
+import id.nesd.rcache.RCache
+import id.nesd.rcache.demo.contracts.ClearContract
+import id.nesd.rcache.demo.utils.LogActionType
+import id.nesd.rcache.demo.utils.LogManager
+
 
 enum class ClearType(val stringValue: String) {
     COMMON("All General Data"),
@@ -7,5 +12,18 @@ enum class ClearType(val stringValue: String) {
     ALL("All Data")
 }
 
-class ClearModel {
+class ClearModel : ClearContract.Model {
+    override fun clear(type: ClearType) {
+        when (type) {
+            ClearType.COMMON -> RCache.common.clear()
+            ClearType.CREDENTIALS -> RCache.credentials.clear()
+            ClearType.ALL -> RCache.clear()
+        }
+
+        addToLog(type)
+    }
+
+    private fun addToLog(type: ClearType) {
+        LogManager.instance.add(action = LogActionType.CLEAR, value = "Clear ${type.stringValue}")
+    }
 }
