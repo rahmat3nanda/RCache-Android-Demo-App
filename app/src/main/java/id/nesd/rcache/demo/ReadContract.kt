37c248a32c453f9b65bbdb94ac686acca7705fbb
Rahmat Trinanda <rahmat3nanda@gmail.com>
2024-09-03 22:39:46 +0700

    Create read contract

---
+++ app/src/main/java/id/nesd/rcache/demo/ReadContract.kt
@@ -0,0 +1,19 @@
+package id.nesd.rcache.demo
+
+import id.nesd.rcache.demo.models.DataType
+import id.nesd.rcache.demo.models.KeyItem
+import id.nesd.rcache.demo.models.StorageType
+
+interface ReadContract {
+    interface View {
+        fun readResult(string: String)
+    }
+
+    interface Model {
+        fun read(key: KeyItem, storageType: StorageType, dataType: DataType)
+    }
+
+    interface Presenter {
+        fun read(key: KeyItem, storageType: StorageType, dataType: DataType)
+    }
+}
